84a71fcc6a13cf7d8d9c10ec9ddf4e050d74a00b
Kragov <robrozdestvensky@gmail.com>
2018-09-16 21:45:28 +0300

    this is sad, but not all tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let counter = 0;
+        if (bracketsConfig[i][0] !== bracketsConfig[i][1]) {
+            for (let j = 0; j < str.length; j++) {
+                if (str[j] === bracketsConfig[i][0]) {
+                    counter++;
+                }
+                else if (str[j] === bracketsConfig[i][1]) {
+                    counter--;
+                }
+                if (counter < 0) {
+                    return false;
+                }
+            }
+            if (counter > 0) {
+                return false
+            }
+        }
+        else {
+            for (let j = 0; j < str.length; j++) {
+                if (str[j] === bracketsConfig[i][0]) {
+                    counter++;
+                }
+            }
+            if ((counter % 2) !== 0) {
+                return false
+            }
+        }
+    }
+    return true;
 }
+
